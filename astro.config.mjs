f089fdd8829ab669324fc7064a54f12a89017858
anasahmd <anasahmad0239@gmail.com>
2023-03-28 16:18:30 +0530

    Add service worker

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from 'astro/config';
+import serviceWorker from 'astrojs-service-worker';
 
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind(), serviceWorker()],
 });
